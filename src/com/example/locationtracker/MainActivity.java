b1460d94a7c865c4cc996f92f78fb0325197c3e6
anksharma90 <anksharma90@gmail.com>
2014-11-09 00:06:20 -0500

    Immersive Mode.

--- src/com/example/locationtracker/MainActivity.java
+++ src/com/example/locationtracker/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.locationtracker;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,16 +19,31 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private BluetoothAdapter mBluetoothAdapter;
+    
+ // Stops scanning after 10 seconds.
+    private static final long REFRESH_TIME = 5000;
+    private static final int REQUEST_ENABLE_BT = 1;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		final BluetoothManager bluetoothManager =
+		        (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+		mBluetoothAdapter = bluetoothManager.getAdapter();
+		
+		if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+		    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+		}
+		
 		final Button button = (Button) findViewById(R.id.scan);
+		
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                TextView t = (TextView) findViewById(R.id.textView1);
-                t.setText("Scanning...");
+                scanLeDevice(true);
             }
         });
 	}
@@ -42,5 +66,49 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+	        super.onWindowFocusChanged(hasFocus);
+	    if (hasFocus) {
+	    	getWindow().getDecorView().setSystemUiVisibility(
+	                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	                | View.SYSTEM_UI_FLAG_FULLSCREEN
+	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+	}
+	
+	private void scanLeDevice(final boolean enable) {
+        if (enable) {
+        	Timer mTimer = new Timer();
+        	mTimer.scheduleAtFixedRate(new TimerTask() {
+				@Override
+        	    public void run() {
+					mBluetoothAdapter. stopLeScan(mLeScanCallback);
+        	    }
+        	}, 0, REFRESH_TIME);
+
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+    }
+	
+	// Device scan callback.
+	private BluetoothAdapter.LeScanCallback mLeScanCallback =
+	        new BluetoothAdapter.LeScanCallback() {
+	    @Override
+	    public void onLeScan(final BluetoothDevice device, int rssi,
+	            byte[] scanRecord) {
+	    	String msg = "payload = ";
+	    	msg += device.toString() + ".....";
+	    	msg += Integer.toString(rssi) + ".....";
+	    	for (byte b : scanRecord)
+	    	  msg += String.format("%02x ", b);
+	    	System.out.println(msg);
+	   }
+	};
 	
 }
