830d963be5dc433d5c3338a799a7d092e98b20ca
anksharma90 <anksharma90@gmail.com>
2014-11-08 19:53:16 -0500

    Sample Button

--- src/com/example/locationtracker/MainActivity.java
+++ src/com/example/locationtracker/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +14,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Button button = (Button) findViewById(R.id.scan);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView t = (TextView) findViewById(R.id.textView1);
+                t.setText("Scanning...");
+            }
+        });
 	}
 
 	@Override
@@ -31,4 +42,5 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
 }
